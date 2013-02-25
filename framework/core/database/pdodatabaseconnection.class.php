4dcd26690864efb37d4005142aef2a89ea9eefcc
Rogier Pennink <rogierpennink@gmail.com>
2013-02-25 18:25:41 +0100

    Implemented all IDatabaseConnection methods in PDODatabaseConnection, except for the query method.

--- framework/core/database/pdodatabaseconnection.class.php
+++ framework/core/database/pdodatabaseconnection.class.php
@@ -12,13 +12,32 @@ class PDODatabaseConnection extends Database\IDatabaseConnection
 
 	private $isConnected;
 
-	public function __construct() {
-		$this->dsn 			= "";
-		$this->pdo 			= null;
-		$this->isConnected 	= false;
+	private $host;
 
+	private $user;
+
+	private $pass;
+
+	private $dbName;
+
+	public function __construct() {
+		$this->reset();
 	}
-	// Server connection related
+
+	public function reset() {
+		$this->dsn 			= "";
+		$this->isConnected 	= false;
+		$this->host 		= "";
+		$this->user 		= "";
+		$this->pass 		= "";
+		$this->dbName 		= "";
+
+		
+		if ( !is_null($this->pdo) ) {
+			unset($this->pdo);
+			$this->pdo = null;
+		}
+	}
 
 	/**
 	 * @access public
@@ -26,7 +45,7 @@ class PDODatabaseConnection extends Database\IDatabaseConnection
 	 * @param $user 		Username with which to connect
 	 * @param $pass 		The password that belongs to the $username
 	 * @param $dbName 		The name of the database to connect to
-	 * @return bool 		true if connection was established, false otherwise
+	 * @return void
 	 * @throws DatabaseConnectionException
 	 */
 	public function connect($host, $user, $pass, $dbName) {
@@ -35,7 +54,20 @@ class PDODatabaseConnection extends Database\IDatabaseConnection
 			throw new Exception\Database\DatabaseConnectionException($message, $host, $user, $pass, $dbName);
 		}
 
+		if ( $this->dsn == "" ) {
+			$message = "DSN was not set.";
+			throw new Exception\Database\DatabaseConnectionException($message, $host, $user, $pass, $dbName);
+		}
 
+		try {
+			$this->pdo = new PDO($this->dsn);
+		}
+		catch (\PDOException $e) {
+			$message = "An internal error occurred during the connection attempt: ". $e->getMessage() ." (". $e->getCode() .")";
+			throw new Exception\Database\DatabaseConnectionException($message, $host, $user, $pass, $dbName);
+		}
+
+		$this->isConnected = true;
 	}
 
 	/**
@@ -53,19 +85,88 @@ class PDODatabaseConnection extends Database\IDatabaseConnection
 		}
 
 		unset($this->pdo);
+		$this->pdo = null;
+
+		$this->isConnected = false;
 	}
 
-	function isConnected();
+	/**
+	 * Returns whether or not the database connection is currently in a connected state.
+	 *
+	 * @access public
+	 * @return bool 		true if connected, false otherwise
+	 */
+	public function isConnected() {
+		return !is_null($this->pdo) && $this->isConnected;
+	}
 
-	// Transactions
-	function transactionStart();
-	function transactionRollback();
-	function transactionCommit();
+	/**
+	 * @access public
+	 * @return bool 		true if a transaction is currently active (when autocommit is not on)
+	 * @throws DatabaseNotConnectedException
+	 */
+	public function isInTransaction() {
+		$this->checkIfConnected();
 
-	// Setters / getters
-	function getHost();
-	function getUser();
-	function getPass();
+		// Apparently returns integers, so transform to bool
+		return $this->pdo->inTransaction() ? true : false;
+	}
+
+	/**
+	 * @access public
+	 * @return bool 		true if transaction successfully started, false otherwise
+	 * @throws DatabaseNotConnectedException
+	 */
+	public function transactionStart() {
+		$this->checkIfConnected();
+
+		return $this->pdo->beginTransaction() ? true : false;
+	}
+
+	/**
+	 * Rolls back the current transaction, discarding any changes. Note, not all storage engines
+	 * support transactions and in those cases this method may return true even if the rollback
+	 * didn't actually happen.
+	 *
+	 * @access public
+	 * @return bool 		true if rollback was successful, false otherwise
+	 * @throws DatabaseNotConnectedException
+	 */
+	public function transactionRollback() {
+		$this->checkIfConnected();
+
+		return $this->pdo->rollBack() ? true : false;
+	}
+
+	/**
+	 * Commits the current transaction, finalizing any changes.
+	 * 
+	 * @access public
+	 * @return bool 		true if commit was successful, false otherwise
+	 * @throws DatabaseNotConnectedException
+	 */
+	public function transactionCommit() {
+		$this->checkIfConnected();
+
+		return $this->pdo->commit() ? true : false;
+	}
+
+	
+	public function getHost() {
+		return $this->host;
+	}
+
+	public function getUser() {
+		return $this->user;
+	}
+
+	public function getPass() {
+		return $this->pass;
+	}
+
+	public function getDbName() {
+		return $this->dbName;
+	}
 
 	// 
 	function query($sql, array $args = array());
@@ -79,6 +180,19 @@ class PDODatabaseConnection extends Database\IDatabaseConnection
 	 */
 	protected function setDSN($dsn) {
 		$this->dsn = $dsn;
+	}
+
+	/**
+	 * Helper method that throws an exception if there is no connection with the database.
+	 *
+	 * @access private
+	 * @return void
+	 * @throws DatabaseNotConnectedException
+	 */
+	private function checkIfConnected() {
+		if ( !$this->isConnected() ) {
+			throw new Exception\Database\DatabaseNotConnectedException("Not connected to any database.");
+		}
 	}
 }
 ?>
